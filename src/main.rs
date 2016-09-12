7c79d91c2457c8cc6cf51ed4f65158eab39c7076
Andrew <andrew.frailing@promessinc.com>
2016-09-12 12:33:02 -0400

    Updated to include a noninteractive mode

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,36 @@ fn relay_stream_async<F: Read + std::marker::Send + 'static, T: Write + std::mar
     })
 }
 
+fn read_until<T: Read>(mut stream: T, sentinel: &str) -> String {
+    let mut result = String::new();
+    let mut buffer: [u8; 256] = [0; 256];
+    while !result.ends_with(sentinel) { 
+        match stream.read(&mut buffer) {
+            Ok(amount_read) => { 
+                if amount_read == 0 { break; }
+                else { result.push_str(str::from_utf8(&buffer[..amount_read]).unwrap()); }
+            },
+            Err(_) => { break; }
+        };
+    }
+    result
+}
+
+fn retrieve_command(mut stream: TcpStream) -> String {
+    let mut result = read_until(stream.try_clone().unwrap(), "\0");
+    let _ = result.pop();
+    let _ = stream.write_all(b"Connected");
+    let _ = stream.flush();
+    result
+}
+
+fn send_command(mut stream: TcpStream, options: &str) {
+    let _ = stream.write_all(options.as_bytes());
+    let _ = stream.write_all(b"\0");
+    let _ = stream.flush();
+    let _ = read_until(stream, "Connected");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let is_server = args.len() == 1;
@@ -48,23 +78,41 @@ fn main() {
 
         println!("Listening on: {}", listener.local_addr().unwrap());
         loop {
-            let (stream, peer_addr) = listener.accept().unwrap();
-            println!("Connected to: {}", peer_addr);
+            let (mut stream, peer_addr) = listener.accept().unwrap();
+            let command = retrieve_command(stream.try_clone().unwrap());
+            
+            if command.len() > 0 {
+                let process = Command::new("cmd")
+                                          .arg(format!("/c {}", command))
+                                          .stdin(Stdio::piped())
+                                          .stdout(Stdio::piped())
+                                          .stderr(Stdio::piped())
+                                          .spawn()
+                                          .unwrap();
+                                          
+                let output = process.wait_with_output().unwrap();
+                let _ = stream.write_all(&output.stdout);
+                let _ = stream.flush();
+                let _ = stream.write_all(&output.stderr);
+                let _ = stream.flush();
+                let _ = stream.shutdown(Shutdown::Both);
+            } else {
+                println!("Connected to: {}", peer_addr);
+                let mut process = Command::new("cmd")
+                                          .arg("/Q") //turn off the echo
+                                          .stdin(Stdio::piped())
+                                          .stdout(Stdio::piped())
+                                          .stderr(Stdio::piped())
+                                          .spawn()
+                                          .unwrap();
 
-            let mut process = Command::new("cmd")
-                                      .arg("/Q") //turn off the echo
-                                      .stdin(Stdio::piped())
-                                      .stdout(Stdio::piped())
-                                      .stderr(Stdio::piped())
-                                      .spawn()
-                                      .unwrap();
-
-            relay_stream_async(process.stderr.take().unwrap(), stream.try_clone().unwrap());
-            relay_stream_async(process.stdout.take().unwrap(), stream.try_clone().unwrap());
-            let handle = relay_stream_async(stream.try_clone().unwrap(), process.stdin.take().unwrap());
-            let _ = handle.join();
-            let _ = stream.shutdown(Shutdown::Both);
-            println!("Client disconnected");
+                relay_stream_async(process.stderr.take().unwrap(), stream.try_clone().unwrap());
+                relay_stream_async(process.stdout.take().unwrap(), stream.try_clone().unwrap());
+                let handle = relay_stream_async(stream.try_clone().unwrap(), process.stdin.take().unwrap());
+                let _ = handle.join();
+                let _ = stream.shutdown(Shutdown::Both);
+                println!("Client disconnected");
+            }
         }
     } else {
         let addr: SocketAddr = match args[1].parse() {
@@ -83,11 +131,18 @@ fn main() {
             }
         };
         
-        println!("Connected");
-
-        relay_stream_async(io::stdin(), stream.try_clone().unwrap());
-        let handle = relay_stream_async(stream, io::stdout());
-        let _ = handle.join();
-        println!("Disconnected");
+        if args.len() > 2 {
+            send_command(stream.try_clone().unwrap(), &args[2..].join(" "));
+            
+            let handle = relay_stream_async(stream, io::stdout());
+            let _ = handle.join();
+        } else {
+            send_command(stream.try_clone().unwrap(), "");
+            
+            relay_stream_async(io::stdin(), stream.try_clone().unwrap());
+            let handle = relay_stream_async(stream, io::stdout());
+            let _ = handle.join();
+            println!("Disconnected");
+        }
     }
 }
